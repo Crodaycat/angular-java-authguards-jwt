1f2e108f35c8ea7ce8f07bd4b280cccf9fff3b34
Luis giraldo <luis.giraldo10@udea.edu.co>
2019-05-25 02:16:25 -0500

    Feat: se agregan allowed roles

--- backend/src/main/java/com/systems/secured/api/service/CharacterService.java
+++ backend/src/main/java/com/systems/secured/api/service/CharacterService.java
@@ -8,6 +8,7 @@ package com.systems.secured.api.service;
 import com.systems.secured.api.bl.CharacterBl;
 import com.systems.secured.api.dto.CharacterDto;
 import java.util.Collection;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -19,18 +20,19 @@ import javax.ws.rs.core.MediaType;
  *
  * @author luis_
  */
-
 @Path("/characters")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class CharacterService 
 {
+    @RolesAllowed("user")
     @GET
     public Collection<CharacterDto> getAll() 
     {
         return CharacterBl.getInstance().getAll();
     }
     
+    @RolesAllowed("user")
     @GET
     @Path("/{id}")
     public CharacterDto getById(@PathParam("id") int id) 
