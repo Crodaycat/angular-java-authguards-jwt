421bb13c57922cf2f437eac99f6853e96d25c452
Santiago Sanmartín <desarrolloingenieria10@udea.edu.co>
2019-05-24 20:28:28 -0500

    Feat: creado botón con angular-material sin funcionalidad

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import { AppViewCharactersComponent } from './app-view-characters/app-view-chara
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
